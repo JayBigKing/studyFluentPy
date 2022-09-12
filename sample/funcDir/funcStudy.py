003b1b6890c6523032515df2c6916d4c3850f478
jay.zhu <873442494@qq.com>
2022-09-12 16:06:52 +0800

    NEW: study 函数式编程
    WHY: study 函数式编程
    
    WHAT: study 函数式编程
    
    TEST: pass

--- sample/funcDir/funcStudy.py
+++ sample/funcDir/funcStudy.py
@@ -76,12 +76,47 @@ def test5():
     '''
     from inspect import signature
     sig = signature(testAnnotation)
+    print(str(sig))
     print(sig.return_annotation)
     print(sig.parameters)
     print(testAnnotation('张', 9))
 
+def test6(choose = 0):
+    if choose == 0:
+        from operator import itemgetter
+        list0 = [i for i in range(10)]
+        ig0 = itemgetter(0)
+        ig1 = itemgetter(0, 1)
+        print(ig0(list0))
+        print(ig1(list0))
+    elif choose == 1:
+        from collections import namedtuple
+        from operator import attrgetter
+        hhName = namedtuple("hhName", "a b c")
+        ag0 = attrgetter("a")
+        ag1 = attrgetter("a", "b")
+        hh0 = hhName(0, "666", 2)
+        print(ag0(hh0))
+        print(ag1(hh0))
+    elif choose == 2:
+        from operator import methodcaller
+        s = 'The time has come'
+        upacase = methodcaller('upper')
+        print(upacase(s))                   #s对象必须有这个方法才行哦
+        hiphenate = methodcaller('replace', ' ', '-')
+        print(hiphenate(s))
+    else:
+        from functools import partial
+        from operator import mul
+        triple = partial(mul, 3)
+        list0 = [triple(i) for i in range(10)]
+        print(list0)
+
+
+
+
 def main():
-    test5()
+    test6(3)
 
 if __name__ == '__main__':
     main()
